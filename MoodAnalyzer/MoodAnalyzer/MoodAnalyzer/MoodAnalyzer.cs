e6fc02bdca449ec42d8ae8a269eb22e1bedc08a5
sahiba <sahiba1214@gmail.com>
2022-01-08 16:21:08 +0530

    [sahiba1214] add file MoodAnalyseHappyOrSad

---
+++ MoodAnalyzer/MoodAnalyzer/MoodAnalyzer/MoodAnalyzer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MoodAnalyzer
+{
+    public class MoodAnalyser
+    {
+        string ne;
+        public string AnalyzeMood(string Massage)
+        {
+            if (Massage.Contains("Sad"))
+                return ne = "Sad";
+            else
+                return "Happy";
+        }
+    }
+}
---
+++ MoodAnalyzer/MoodAnalyzer/MoodAnalyzerTesting/MoodAnalyzerTest.cs
@@ -0,0 +1,30 @@
+﻿using MoodAnalyzer;
+using NUnit.Framework;
+
+namespace MoodAnalyzerTesting
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            MoodAnalyser moodAnalyser = new MoodAnalyser();
+            string expected = "Sad";
+            string actual = moodAnalyser.AnalyzeMood("I am in Sad mood");
+            Assert.AreEqual(expected, actual);
+        }
+        public void Test2()
+        {
+            MoodAnalyser moodAnalyser = new MoodAnalyser();
+            string expected = "Happy";
+            string actual = moodAnalyser.AnalyzeMood("I am in Any mood");
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
